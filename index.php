842ee8c53870f27861c522e6589a0a87401fed39
429273-7 <429273-7>
2020-11-05 09:57:35 +0300

    Lab3 released

--- index.php
+++ index.php
@@ -1,34 +1,21 @@
 <?php
-class A{
-    function equ ($a, $b){
-        $x = -($b/$a);
-        $this -> x = $x;
-        return $x;
-    }
-}
-class B extends A{
-    protected function dis($a, $b, $c){
-        $d = ($b**2)-4*$a*$c;
-        return $d;
-    }
-    public function check ($a, $b, $c) {
-        $x = $this -> dis($a, $b, $c);
-        if ($x > 0 ) {
-            return $this -> x = array (-($b + sqrt($x)/2*$a),
-                -($b - sqrt($x)/2*$a)
-            );
-        }
-        if ($x < 0) {
-            return $this -> x = NULL;
-        }
-        return $this -> x = -($b/2*$a);
 
-    }
-}
-$a = new A();
-$b = new B();
-$a -> equ(2,4);
-$a = var_dump($a);
-$b -> check(2,4,2);
-$b = var_dump($b);
+
+use zhigulin\MyLog;
+use zhigulin\Square;
+use zhigulin\ZhigulinException;
+
+include "core/EquationInterface.php";
+include "core/LogInterface.php";
+include "core/LogAbstract.php";
+include "zhigulin/Linear.php";
+include "zhigulin/Square.php";
+include "zhigulin/MyLog.php";
+include "zhigulin/ZhigulinException.php";
+
+ini_set("display_errors", 1);
+error_reporting(-1);
+
+zhigulin\MyLog::log("1234");
+zhigulin\MyLog::write();
 ?>
---
+++ zhigulin/MyLog.php
@@ -0,0 +1,24 @@
+<?php
+namespace zhigulin;
+use core\LogAbstract;
+use core\LogInterface;
+
+class MyLog extends LogAbstract implements LogInterface{
+
+    public function _log(String $str){
+        $this->log[]=$str;
+    }
+    public static function log($str){
+        self::Instance()->_log($str);
+    }
+    public function _write(){
+        foreach($this->log as $value){
+            echo $value."\n";
+        }
+    }
+    public static function write(){
+        self::Instance()->_write();
+    }
+
+
+}
